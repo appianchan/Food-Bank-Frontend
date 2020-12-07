8104a7fe974f5ff273b8760afd2831a3141569de
Anthony Chan <appianchan@gmail.com>
2020-12-07 15:43:28 -0800

    making basic framework for app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Places from './Components/places'
 
 function App() {
   return (
     <div className="App">
       <header className="Title">
-        Food Finder
+        Food Bank Finder
       </header>
+      <Places />
     </div>
   );
 }
---
+++ src/Components/SearchBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default class SearchBar extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            zipcode: 94586
+        }
+        this.changeZipCodeInState = this.changeZipCodeInState.bind(this);
+        this.toAnalyze = this.toAnalyze.bind(this);
+    }
+    changeZipCodeInState(event){
+        this.setState({zipcode: event.target.value})
+        event.preventDefault();
+    }
+
+    toAnalyze(){
+        this.props.callback(this.state.zipcode);
+    }
+
+    render(){
+        return(
+            <div>
+                <div>
+                    <input onchange={this.changeZipCodeInState} type="text" name="zipcode" />
+                </div>
+                <button onClick={this.toAnalyze}>Change Zip Code</button>
+                
+            </div>
+        )
+    }
+}
